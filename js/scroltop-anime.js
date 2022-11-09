6220be624be0b89c21e40736ac9c4597a8933e52
Otávio Luiz <101069995+OtavioLuizVentRox@users.noreply.github.com>
2022-11-09 16:43:57 -0300

    Add files via upload

---
+++ js/scroltop-anime.js
@@ -0,0 +1,18 @@
+const scrollanima = document.querySelector('[data-anima="scroll"]');
+
+const metadeWindow = window.innerHeight * 1.7;
+
+function animarScroll() {
+
+    const topoItem = scrollanima.getBoundingClientRect().top;
+
+    const itemVisivel = topoItem - metadeWindow < 0;
+
+
+    if (itemVisivel) {
+        scrollanima.classList.add('show-button');
+    } else { scrollanima.classList.remove('show-button'); }
+
+
+}
+window.addEventListener("scroll", animarScroll);
